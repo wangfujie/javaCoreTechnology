0c61653592291237d83a62e3f0c32d1ace68ab92
wangfj <784727590@qq.com>
2017-11-23 11:21:46 +0800

    关于集合的一点内容

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
-    public List<String> testList(){
-        //线程安全的List
-        List<String> list = new CopyOnWriteArrayList<>();
-        for (int i = 0 ; i < 10 ; i++) {
-            list.add(String.valueOf(i));
-        }
-        return list;
-    }
-
-    public static void testCollection(){
-        //有序的set集合，它继承于HashSet，仅仅比HashSet多个有序
-        Set<String> set = new LinkedHashSet<>();
-        for (int i = 0 ; i < 1000 ; i++) {
-            set.add(String.valueOf(i));
-        }
-        set.forEach(System.out::println);
-    }
-
-    public static void testMap(){
-        //有序的Map，LinkedHashMap (它是HashMap的子类，继承了HashMap)
-        Map<String, Object> map = new LinkedHashMap<>();
-        map.put("AAA","1");
-        map.put("BBB","2");
-        map.put("CCC","3");
-        map.put("DDD","4");
-        map.put("EEE","5");
-        map.put("FFF","6");
-        map.put("GGG","7");
-        Set<String> keySet = map.keySet();
-        //jdk1.8新特性forEach循环
-        keySet.forEach(key -> {
-            System.out.println(key + " <--> " + map.get(key));
-        });
-    }
 }
