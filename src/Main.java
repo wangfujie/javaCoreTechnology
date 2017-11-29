40febac923380f6b3d7e3d7f3886aee9bd1a18ae
wangfj <784727590@qq.com>
2017-11-29 11:11:34 +0800

    测试提交

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static strictfp void main(String[] args) {
         //用逗号分割字符串“hello”
         System.out.println(String.join(",","hello".split("")));
+        //测试类方法调用
         StringTest.formatTest();
     }
 }
